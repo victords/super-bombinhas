39e6116e527aac9deee41b450afb7453f400069b
Victor David Santos <victordavidsantos@gmail.com>
2014-02-20 22:48:59 -0300

    Implementação de outros modos de alinhamento de texto (esquerda, direita, centralizado); encapsulamento para escrever linha única.

--- text.rb
+++ text.rb
@@ -1,30 +1,52 @@
 class TextHelper
-	def initialize font, line_spacing
+	def initialize font, line_spacing = 0
 		@font = font
 		@line_spacing = line_spacing
 	end
 	
-	def write_breaking text, x, y, width, justified = false
+	def write_line text, x, y, mode = :left
+		rel =
+			case mode
+			when :left then 0
+			when :center then 0.5
+			when :right then 1
+			else 0
+			end
+		@font.draw_rel text, x, y, 0, rel, 0
+	end
+	
+	def write_breaking text, x, y, width, mode = :left
 		text.split("\n").each do |p|
-			if justified
+			if mode == :justified
 				y = write_paragraph_justified p, x, y, width
 			else
-				y = write_paragraph p, x, y, width
+				rel =
+					case mode
+					when :left then 0
+					when :center then 0.5
+					when :right then 1
+					else 0
+					end
+				y = write_paragraph p, x, y, width, rel
 			end
 		end
 	end
 	
-	def write_paragraph text, x, y, width
-		new_x = x
+	def write_paragraph text, x, y, width, rel
+		line = ""
+		line_width = 0
 		text.split(' ').each do |word|
 			w = @font.text_width word
-			if new_x + w > x + width
-				new_x = x
+			if line_width + w > width
+				@font.draw_rel line.chop, x, y, 0, rel, 0
+				line = ""
+				line_width = 0
 				y += @font.height + @line_spacing
 			end
-			@font.draw word, new_x, y, 0
-			new_x += @font.text_width "#{word} "
+			line += "#{word} "
+			line_width += @font.text_width "#{word} "
 		end
+		@font.draw_rel line.chop, x, y, 0, rel, 0 if not line.empty?
 		y + @font.height + @line_spacing
 	end
 	
