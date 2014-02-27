a99657074804527c3576b15885ea25ac1ce747a2
Victor David Santos <victordavidsantos@gmail.com>
2014-02-26 23:03:41 -0300

    Criação do cursor para menus e teste de animação.

--- menu.rb
+++ menu.rb
@@ -5,6 +5,10 @@ class Menu
 	def initialize
 		@bg = Res.img :bg_start1, true, false, ".jpg"
 		@title = Res.img :other_title, true
+		@cursor = Sprite.new 180, 288, :other_cursor, 4, 2
+		@cursor_indices = [0, 1, 2, 3, 4, 5, 6, 7]
+		@cursor_timer = 0
+		@cursor_state = 0
 	end
 	
 	def update
@@ -13,11 +17,21 @@ class Menu
 			G.player = Player.new
 			G.state = :map
 		end
+		@cursor.animate @cursor_indices, 5
+		@cursor_timer += 1
+		if @cursor_timer == 10
+			if @cursor_state < 3; @cursor.x += @cursor_state + 1
+			else; @cursor.x -= 6 - @cursor_state; end
+			@cursor_state += 1
+			@cursor_state = 0 if @cursor_state == 6
+			@cursor_timer = 0
+		end
 	end
 	
 	def draw
 		@bg.draw 0, 0, 0
 		@title.draw 0, 0, 0
+		@cursor.draw
 		G.font.draw_rel "Press 'A' to start", 400, 300, 0, 0.5, 0.5, 2, 2, 0xff000000
 	end
 end
