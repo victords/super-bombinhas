e5dd263648c68bfe14e51aca8d69beb25ba5d104
victor <victordavidsantos@gmail.com>
2020-03-29 11:29:15 -0300

    Update debian package

--- deb.sh
+++ deb.sh
@@ -2,10 +2,7 @@
 
 # Aleva Deb Generation Script
 
-cp -R deb deb_temp
-sudo chmod 6775 -R deb_temp
-sudo chmod a-s -R deb_temp/DEBIAN
-sudo chmod a+x deb_temp/DEBIAN/postinst
-sudo chown root: -R deb_temp
-dpkg -b deb_temp .
-sudo rm -rf deb_temp
+ruby bundle.rb
+mv sb.rb deb/opt/aleva-games/super-bombinhas/
+cp -r data deb/opt/aleva-games/super-bombinhas/
+dpkg -b deb super-bombinhas-0.1.0.deb
