bfe5ceedb8ff23f69162a2890a6183a1bf89e29c
Victor David Santos <victordavidsantos@gmail.com>
2025-03-09 15:13:27 -0300

    Fix deb package postinst and packaging script

--- deb.sh
+++ deb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ruby bundle.rb
+mkdir -p deb/opt/vds-games/super-bombinhas
 mv sb.rb deb/opt/vds-games/super-bombinhas/
 cp -r data deb/opt/vds-games/super-bombinhas/
 dpkg -b deb super-bombinhas-1.5.2.deb
