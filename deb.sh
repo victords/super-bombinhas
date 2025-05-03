d2683c01becde2161bf5ff72076623e10e65a3d8
Victor David Santos <victordavidsantos@gmail.com>
2025-05-03 18:10:46 -0300

    [v1.5.4] Add indonesian translation.

--- deb.sh
+++ deb.sh
@@ -4,4 +4,4 @@ ruby bundle.rb
 mkdir -p deb/opt/vds-games/super-bombinhas
 mv sb.rb deb/opt/vds-games/super-bombinhas/
 cp -r data deb/opt/vds-games/super-bombinhas/
-dpkg -b deb super-bombinhas-1.5.3.deb
+dpkg -b deb super-bombinhas-1.5.4.deb
